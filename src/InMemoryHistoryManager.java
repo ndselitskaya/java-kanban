a3cf1802c03fb6a7a378aa1aae185a5d30151002
Nadya Selickaya <nadinehm2210@mail.ru>
2025-07-16 00:02:25 +0400

    добавление алгоритма со связным списком и HashMap для истории просмотра

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,29 +1,78 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager{
+    private static class Node {
+        Task task;
+        Node prev;
+        Node next;
 
-    private final List<Task> historyTasks= new ArrayList<>();
+        Node(Task task) {
+            this.task = task;
+        }
+    }
+
+    private final Map<Integer, Node> historyMap = new HashMap<>();
+    private Node head;
+    private Node tail;
 
     //метод добавления просмотренной задачи в кеш
     @Override
     public void add(Task task) {
-        historyTasks.add(task);
-        if (historyTasks.size()>10){
-            historyTasks.removeFirst();
-        }
+        if (task == null) return; //удаляем старое вхождение, если есть
+        remove(task.getId());
+        Node newNode = new Node(task);
+        linkLast(newNode);
+        historyMap.put(task.getId(), newNode);
     }
 
-    //метод просмотра истории задач
+    //метод просмотра истории задач (аналог getTasks(), поэтому его нет)
     @Override
     public List<Task> getHistory(){
-        System.out.println(historyTasks);
-        return historyTasks;
+        List<Task> tasks = new ArrayList<>();
+        Node current = head;
+        while (current != null) {
+            tasks.add(current.task);
+            current = current.next;
+        }
+        return tasks;
     }
 
     //метод удаления вновь просмотренной задачи
     @Override
     public void remove(int id) {
-       historyTasks.remove(id);
+        Node node = historyMap.get(id);
+        if (node != null) {
+            removeNode(node);
+            historyMap.remove(id);
+        }
+    }
+
+    //метод добавления задачи в конец списка
+    private void linkLast(Node node) {
+        if (head == null) {
+            head = node;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+        }
+        tail = node;
+    }
+
+    //метод удаления ноды в HashMap
+    private void removeNode(Node node) {
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+        }
     }
 }
